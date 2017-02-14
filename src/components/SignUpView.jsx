4e4d5c5a186f054f0710b54caf8801cfb59b7f1e
omrik <omri.kochavi@amdocs.com>
2017-02-14 16:56:51 +0200

    changed text on signup

--- src/components/SignUpView.jsx
+++ src/components/SignUpView.jsx
@@ -19,9 +19,9 @@ export default class SignUpView extends React.Component {
 		return (
 			<div className='sign-up-view'>
 				<div className='sign-up-view--wrapper'>
-					<h1 className='sign-up-view--heading'>Enter Username</h1>
+					<h1 className='sign-up-view--heading'>Enter Your Name</h1>
 					<input className='sign-up-view--input' onKeyDown={e => this.handleKeyDown(e)} value={this.state.newUserName} onChange={e => this.setState({newUserName: e.target.value})} />
-					<button className='sign-up-view--button' onClick={() => this.props.onCreateUser(this.state.newUserName)}>Sign Up</button>
+					<button className='sign-up-view--button' onClick={() => this.props.onCreateUser(this.state.newUserName)}>Start Listening!</button>
 				</div>
 			</div>
 		);
